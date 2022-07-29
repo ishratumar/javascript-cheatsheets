18a15c2fe8d5f6c1fb682855c326aed5de8fbef2
Ishrat <47534248+ishratUmar18@users.noreply.github.com>
2022-07-29 15:05:43 +0500

    Create IIFE-JvaScript.js

---
+++ IIFE-JvaScript.js
@@ -0,0 +1,22 @@
+
+
+// function statement
+
+                function(){
+                  console.log("Hello World");
+                }
+
+// immediately invoked function expression
+
+// without parameters  
+
+              (function(){  
+                 console.log("Hello World");
+               })();                        
+
+// with parameters
+
+             (function(name){
+                console.log("Hello" + name);  
+             })("World");   
+       
