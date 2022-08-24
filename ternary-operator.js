21a2d6ed8b220ef91d0aca61cf19841b7c181cf3
Ishrat <47534248+ishratUmar18@users.noreply.github.com>
2022-08-24 23:33:21 +0500

    Create ternary-operator.js

---
+++ ternary-operator.js
@@ -0,0 +1,18 @@
+// if-else statement
+const isGoingOut = true
+
+let answer 
+
+if(isGoingOut === true) {
+
+answer = "Yes"
+
+} else {
+  
+answer = "No"
+}
+
+// Ternary Operator👍🏻
+const isGoingOut = true
+
+let answer = isGoingOut  ? "Yes" : "No"
